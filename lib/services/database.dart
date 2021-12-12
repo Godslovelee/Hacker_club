d1ccbbe601a9ec8ee16ac0100a9e616d186f6d01
Godslovelee <godslovelee@yahoo.com>
2021-12-12 23:42:19 +0100

    ""

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -47,7 +47,7 @@ class OurDatabase {
     try {
       members.add(userUid);
 
-      DocumentReference _docRef = await _firestore.collection("groups").add({
+      DocumentReference _docRef = await _firestore.collection("groupsf").add({
         'name': groupName,
         'leader': userUid,
         'members': members,
