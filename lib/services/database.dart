714b87367669a20aaeebd509d80d5268d95c6e82
Godslovelee <godslovelee@yahoo.com>
2021-12-19 20:55:40 +0100

    ""

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -33,7 +33,7 @@ class OurDatabase {
       retVal.uid = uid;
       retVal.fullName = _docSnapshot.data["fullName"];
       retVal.email = _docSnapshot.data["email"];
-      retVal.aacountCreated = _docSnapshot.data["accountCreddsated"];
+      retVal.aacountCreated = _docSnapshot.data["accountCrdsdeddsated"];
       retVal.groupId = _docSnapshot.data["groupId"];
     } catch (e) {}
 
